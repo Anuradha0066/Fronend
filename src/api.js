87f8dc54eb4250fd45ba032cc4c65eb943b35df5
Anuradha0066 <singhanuradha0066@gmail.com>
2025-12-07 23:05:16 +0530

    c

--- src/api.js
+++ src/api.js
@@ -2,10 +2,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://backend-nine-mocha-94.vercel.app',  // change if different
+  baseURL: 'https://backend-nine-mocha-94.vercel.app/api',
 });
 
-// If staff logs in and you store token (e.g. in localStorage)
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('staffToken');
   if (token) config.headers.Authorization = `Bearer ${token}`;
